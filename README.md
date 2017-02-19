6c4a80bf1d5b5a70ec2fd95008480afae69fa3e9
Paul Mucur <mudge@mudge.name>
2017-02-19 14:03:04 +0000

    Initial commit of arXiv ID library for Node.js
    Note the regular expressions used in this version of the Identifiers
    library differs slightly from the Ruby and PHP versions as JavaScript
    does not support lookbehinds. Instead, we use capturing groups to ignore
    the boundary before a match.
    
    As this means we're matching a pattern multiple times with capturing
    groups, we have to use `Regexp.prototype.exec()` in a loop until the
    search is exhausted.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Identifiers - arXiv [![Build Status](https://travis-ci.org/altmetric/identifiers-arxiv.svg?branch=master)](https://travis-ci.org/altmetric/identifiers-arxiv)
+
+Extract, validate and normalize [arXiv IDs](https://arxiv.org/help/arxiv_identifier).
+
+**Current version:** 0.1.0  
+**Supported Node.js versions:** 4, 5, 6, 7
+
+## Installation
+
+Add the following to your `package.json` via `yarn add identifiers-arxiv` or `npm install --save identifiers-arxiv`:
+
+```shell
+"identifiers-arxiv": "^0.1.0"
+```
+
+## Usage
+
+```javascript
+const arxiv = require("identifiers-arxiv");
+
+arxiv.extract("math.GT/0309136\narXiv:1501.00001v2");
+//=> ["math.GT/0309136", "1501.00001v2"]
+```
+
+## Other versions
+
+We also maintain versions of this library for [Ruby](https://github.com/altmetric/identifiers) and [PHP](https://github.com/altmetric/php-identifiers).
+
+## License
+
+Copyright © 2017 Altmetric LLP
+
+Distributed under the [MIT License](http://opensource.org/licenses/MIT).
---
+++ tests/arxiv.test.js
@@ -0,0 +1,40 @@
+/* globals test, expect */
+"use strict";
+
+const arxiv = require("../lib/arxiv");
+
+test("extracts pre-2007 arXiv IDs without a scheme", () => {
+    expect(arxiv.extract("Example: math.GT/0309136")).toEqual(["math.GT/0309136"]);
+});
+
+test("extracts pre-2007 arXiv IDs with a scheme", () => {
+    expect(arxiv.extract("arXiv:math.GT/0309136")).toEqual(["math.GT/0309136"]);
+});
+
+test("extracts post-2007 arXiv IDs without a scheme", () => {
+    expect(arxiv.extract("Example: 0706.0001")).toEqual(["0706.0001"]);
+});
+
+test("extracts post-2007 arXiv IDs with a version", () => {
+    expect(arxiv.extract("Example: 1501.00001v2")).toEqual(["1501.00001v2"]);
+});
+
+test("extracts post-2007 arXiv IDs with a scheme", () => {
+    expect(arxiv.extract("Example: arXiv:0706.0001")).toEqual(["0706.0001"]);
+});
+
+test("extracts both pre- and post-2007 arXiv IDs", () => {
+    expect(arxiv.extract("math.GT/0309136\narXiv:1501.00001v2")).toEqual(["math.GT/0309136", "1501.00001v2"]);
+});
+
+test("does not extract IDs from the end of DOIs", () => {
+    expect(arxiv.extract("10.1049/el.2013.3006")).toEqual([]);
+});
+
+test("does not extract IDs from the middle of DOIs", () => {
+    expect(arxiv.extract("10.2310/7290.2014.00033")).toEqual([]);
+});
+
+test("returns an empty array if there are no arXiv IDs", () => {
+    expect(arxiv.extract("No IDs here")).toEqual([]);
+});
